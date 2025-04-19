9c43f8edefd17d3815fa9ecd9cc80879a2dceb55
Raphael Sackey <raphaelsackey99@gmail.com>
2025-04-19 00:58:20 -0400

    feat: implement search functionality and enhance item card layout in browse items page

--- app/(pages)/browseitems/page.tsx
+++ app/(pages)/browseitems/page.tsx
@@ -1,18 +1,60 @@
-import ItemCard from "@/components/ui/itemcard"
+"use client"
 
-export default function BrowseItems(){
-    return(
-        <div className="container mx-auto">
-            browse items
+import ItemCard from "@/components/ui/itemcard";
+import { useState } from "react";
+import { Search } from "lucide-react"; 
 
-            {/* items near by */}
-            <div className=" grid md:grid-cols-2 xl:grid-cols-3 grid-cols-1 gap-2">
-                <ItemCard imgUrl="ds" description="sd" distance="ds" status="Available" name="sdadd"/>
-                <ItemCard imgUrl="ds" description="sd" distance="ds" status="Available" name="sdadd"/>
-                <ItemCard imgUrl="ds" description="sd" distance="ds" status="Available" name="sdadd"/>
-                <ItemCard imgUrl="ds" description="sd" distance="ds" status="Available" name="sdadd"/>
-                <ItemCard imgUrl="ds" description="sd" distance="ds" status="Available" name="sdadd"/>
-            </div>
-        </div>
-    )
+export default function BrowseItems() {
+    const [searchWord, setSearchWord] = useState('')
+	return (
+		<div className='container mx-auto'>
+            <div></div>
+			<div className="relative">
+				<form className="w-full">
+                    <Search className="absolute top-2.5 left-4"/>
+					<input type='text' className="h-10 border w-full rounded pl-13" placeholder= "Search items..." value={searchWord} onChange={(e: React.ChangeEvent<HTMLInputElement>)=> setSearchWord(e.target.value)} />
+				</form>
+			</div>
+
+			{/* items near by */}
+            <h1 className="text-lg">Location: <span className="opacity-50 text-md">Near You</span></h1>
+			<div className=' grid md:grid-cols-2 xl:grid-cols-3 grid-cols-1 gap-2 '>
+				<ItemCard
+					imgUrl='ds'
+					description='sd'
+					distance='ds'
+					status='Available'
+					name='sdadd'
+				/>
+				<ItemCard
+					imgUrl='ds'
+					description='sd'
+					distance='ds'
+					status='Available'
+					name='sdadd'
+				/>
+				<ItemCard
+					imgUrl='ds'
+					description='sd'
+					distance='ds'
+					status='Available'
+					name='sdadd'
+				/>
+				<ItemCard
+					imgUrl='ds'
+					description='sd'
+					distance='ds'
+					status='Available'
+					name='sdadd'
+				/>
+				<ItemCard
+					imgUrl='ds'
+					description='sd'
+					distance='ds'
+					status='Available'
+					name='sdadd'
+				/>
+			</div>
+		</div>
+	);
 }
--- components/ui/itemcard.tsx
+++ components/ui/itemcard.tsx
@@ -1,26 +1,40 @@
+import { TitemCard } from "@/types/C_types";
+import Image from "next/image";
 
-import { TitemCard } from "@/types/C_types"
-import Image from "next/image"
+export default function ItemCard(props: TitemCard) {
+	return (
+		<div className='relative'>
+			{/* Highlight behind card */}
+			<div className='absolute inset-0 top-3 right-3 flex items-center justify-center z-0'>
+				<div className='w-full h-full rounded-lg bg-gradient-to-br from-emerald-400 via-blue-500 to-purple-500  blur-sm opacity-30' />
+			</div>
+			<div className='relative grid w-full h-52 grid-cols-7 p-2 rounded-lg gap-2 items-center max-h-full border z-1 bg-background'>
+				{/* left */}
+				<div className='flex items-center justify-center relative h-full w-full col-span-3'>
+					<Image
+						src='/assets/lawn.jpg'
+						alt='image url'
+						fill
+						className='rounded'
+					/>
+				</div>
 
-export default function ItemCard(props: TitemCard){
-
-    return (
-        <div className="relative grid w-full h-52 grid-cols-7 p-2 rounded-lg gap-2 items-center max-h-full bg-accent">
-            {/* left */}
-            <div className="flex items-center justify-center relative h-full w-full col-span-3">
-                <Image src='/assets/lawn.jpg'  alt="image url" fill className="rounded"/>
-            </div>
-            
-            {/* right */}
-            <div className="col-span-4 max-h-full">
-                <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold opacity-85 truncate">Lawm Mower </h1>
-                <p className="text-md md:text-lg opacity-60 line-clamp-2">here is the descdkad jkdahd hadsu hasd kjasd hdady hadb adjupsdj </p>
-                <h4 className="text-md md:text-lg opacity-60">2 miles </h4>
-                <h4 className="text-md md:text-lg opacity-60">Available</h4>
-            </div>
-
-            <button className="absolute right-2 bottom-1 bg-blue-600 px-7 py-2 rounded text-white">Request</button>
-        
-        </div>
-    )
+				{/* right */}
+				<div className='col-span-4 max-h-full'>
+					<h1 className='text-5xl md:text-5xl lg:text-6xl font-bold opacity-85 truncate'>
+						Lawm Mower{" "}
+					</h1>
+					<p className='text-md md:text-lg opacity-60 line-clamp-2'>
+						here is the descdkad jkdahd hadsu hasd kjasd hdady hadb
+						adjupsdj{" "}
+					</p>
+					<h4 className='text-md md:text-lg opacity-60'>2 miles </h4>
+					<h4 className='text-md md:text-lg opacity-60'>Available</h4>
+				</div>
+				<button className='absolute right-2 bottom-1 bg-blue-600 px-7 py-2 rounded text-white'>
+					Request
+				</button>
+			</div>
+		</div>
+	);
 }
